07df5dbf737f130142b18521069b2825e4fd17ba
estr3llas <nao24nao69nao420@gmail.com>
2024-08-04 21:29:56 -0300

    NtAllocateVirtualMemory

--- src/nt/syscalls.cpp
+++ src/nt/syscalls.cpp
@@ -2,4 +2,19 @@
 // Created by User on 8/4/2024.
 //
 
+#include "Windows.h"
+
 #include "../../headers/nt/syscalls.h"
+
+extern "C" {
+
+    typedef NTSTATUS(NTAPI *NtAllocateVirtualMemory) (
+        HANDLE ProcessHandle,
+        PVOID *BaseAddress,
+        ULONG_PTR ZeroBits,
+        PSIZE_T RegionSize,
+        ULONG AllocationType,
+        ULONG Protect
+        );
+
+}
