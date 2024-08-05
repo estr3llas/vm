31116f8baea247c0edc126a173e5a6567dc8d1f9
estr3llas <nao24nao69nao420@gmail.com>
2024-08-04 21:23:10 -0300

    syscall

---
+++ headers/nt/syscalls.h
@@ -0,0 +1,8 @@
+//
+// Created by User on 8/4/2024.
+//
+
+#ifndef SYSCALLS_H
+#define SYSCALLS_H
+
+#endif //SYSCALLS_H
---
+++ src/nt/syscalls.cpp
@@ -0,0 +1,5 @@
+//
+// Created by User on 8/4/2024.
+//
+
+#include "../../headers/nt/syscalls.h"
