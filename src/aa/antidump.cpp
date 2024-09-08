9e44ba96100b74d7f491167dfa7e1281f178f38d
estr3llas <nao24nao69nao420@gmail.com>
2024-09-07 21:57:47 -0300

    namespaces, namespaces everywhere!!!

--- src/aa/antidump.cpp
+++ src/aa/antidump.cpp
@@ -9,28 +9,30 @@
 #include "../../headers/nt/parser.h"
 #include "../../headers/nt/peb.h"
 
-void IncreaseSizeOfImage(peb::PPEB peb) {
-    const static auto ldr_data = peb->LoaderData;
-    const auto& [flink, blink] = ldr_data->InLoadOrderModuleList;
-    const static auto pe = reinterpret_cast<peb::PLDR_DATA_TABLE_ENTRY>(flink->Flink);
+namespace Antidump {
+    void IncreaseSizeOfImage(peb::PPEB peb) {
+        const static auto ldr_data = peb->LoaderData;
+        const auto& [flink, blink] = ldr_data->InLoadOrderModuleList;
+        const static auto pe = reinterpret_cast<peb::PLDR_DATA_TABLE_ENTRY>(flink->Flink);
 
-    auto pSize = &pe->SizeOfImage;
+        auto pSize = &pe->SizeOfImage;
 
-    *pSize = static_cast<
-        ULONG > (
-            static_cast< INT_PTR > (pe->SizeOfImage + 0x10000000));
-}
+        *pSize = static_cast<
+            ULONG > (
+                static_cast< INT_PTR > (pe->SizeOfImage + 0x10000000));
+    }
 
-void EraseHeader() {
-    DWORD oldProtect = 0;
+    void EraseHeader() {
+        DWORD oldProtect = 0;
 
-    //auto base = getModuleBase2();
+        //auto base = getModuleBase2();
 
-    if (const static auto base = getModuleBase();
-        VirtualProtect(base, PAGE_SIZE, PAGE_READWRITE, &oldProtect)
-        ) {
-        RtlSecureZeroMemory(base, PAGE_SIZE);
+        if (const static auto base = getModuleBase();
+            VirtualProtect(base, PAGE_SIZE, PAGE_READWRITE, &oldProtect)
+            ) {
+            RtlSecureZeroMemory(base, PAGE_SIZE);
 
-        VirtualProtect(base, PAGE_SIZE, oldProtect, &oldProtect);
+            VirtualProtect(base, PAGE_SIZE, oldProtect, &oldProtect);
+            }
     }
 }
